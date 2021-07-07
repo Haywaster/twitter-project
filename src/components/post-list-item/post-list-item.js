d67a5e01f5d3ea8f2092b9a53b3e9f25c9b25477
Vladimir <haywaster3@gmail.com>
2021-07-08 00:28:30 +0700

    Framework without styles

---
+++ src/components/post-list-item/post-list-item.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PostList from '../post-list/post-list';
+
+const PostListItem = () => {
+    return (
+        <li className="app-list-item d-flex justify-content-between">
+            <span className="app-list-item-label">
+                Hello world
+            </span>
+            <div className="d-flex justify-content-center aling-items-center">
+                <button 
+                type="button" className="btn-star btn-sm">
+                    <i className="fa fa-star"></i>
+                </button>
+
+                <button 
+                type="button" className="btn-trash btn-sm">
+                    <i className="fa fa-trash"></i>
+                </button>
+                
+                <i className="fa fa-heart"></i>
+            </div>
+        </li>
+    )
+}
+
+export default PostListItem;
