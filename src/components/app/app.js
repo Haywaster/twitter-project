d0ec90ffcf28ba1e7c15a491d9a4264466427692
Vladimir <haywaster3@gmail.com>
2021-07-09 20:34:08 +0700

    styled-components, delete and add cards

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,25 +7,71 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form/post-add-form';
 
 import './app.css';
-import '../app-header/app-header.css';
-import '../post-add-form/post-add-form.css';
-import '../post-list/post-list.css';
-import '../post-list-item/post-list-item.css';
-import '../post-status-filter/post-status-filter.css';
-import '../search-panel/search-panel.css';
+import styled from 'styled-components';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList/>
-            <PostAddForm/>
-        </div>
-    )
+const AppBlock = styled.div `
+    margin: 0 auto;
+    max-width: 800px;
+`
+export default class App extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+                data : [
+                {label: 'Going to learn React', important: true, id:1},
+                {label: 'This is so good', important: false, id:2},
+                {label: 'I need a break...', important: false, id:3}
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this)
+        this.addItem = this.addItem.bind(this)
+
+        this.maxId = 4;
+    }
+
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+
+            const newArr = [...before, ...after]
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render() {
+        return (
+            <AppBlock className="app">
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList 
+                    onDelete={this.deleteItem}
+                    posts={this.state.data} />
+                <PostAddForm
+                    onAdd={this.addItem}/>
+            </AppBlock>
+        )
+    }
 }
-
-export default App;
